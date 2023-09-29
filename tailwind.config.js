31ff2805dbdc89cb55167fb6fc67087d942ceef9
bimus-github <bimus2022@gmail.com>
2023-09-29 22:02:14 +0500

    adding animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,18 @@ module.exports = {
       boxShadow: {
         bottom: "0px 14px 8px -12px rgba(34, 60, 80, 0.46);",
       },
+      animation: {
+        "flip-load": "flip-load 2s linear infinite",
+      },
+      keyframes: {
+        "flip-load": {
+          "0%": { transform: "rotateX(0deg) rotateY(0deg)" },
+          "25%": { transform: "rotateX(0deg) rotateY(180deg)" },
+          "50%": { transform: "rotateX(180deg) rotateY(180deg)" },
+          "75%": { transform: "rotateX(180deg) rotateY(0deg)" },
+          "100%": { transform: "rotateX(0deg) rotateY(0deg)" },
+        },
+      },
     },
   },
   plugins: [],
