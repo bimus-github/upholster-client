3c1d1f02706df91acfcd3a9cfe140ae73fbf5ce8
bimus-github <bimus2022@gmail.com>
2023-09-29 22:01:51 +0500

    service page

--- src/pages/services/index.tsx
+++ src/pages/services/index.tsx
@@ -1,11 +1,26 @@
 import React from "react";
+import { ServiceInfo, ServicesList, TitleForPage } from "../../components";
 
 function Services() {
+  const [selectedItemName, setSelectedItemName] = React.useState<string>("");
+
   return (
-    <div className="h-[100vh] bg-blue-300" id="services">
-      Services
-    </div>
+    <section className={styles.services} id="services">
+      <TitleForPage title="Xizmatlar" />
+
+      <ServicesList
+        setSelectedItemName={setSelectedItemName}
+        selectedItemName={selectedItemName}
+      />
+
+      <ServiceInfo selectedItemName={selectedItemName} />
+    </section>
   );
 }
 
 export default Services;
+
+const styles = {
+  services:
+    "min-h-[100vh] py-5 px-2 flex flex-col items-start gap-10 bg-slate-100",
+};
