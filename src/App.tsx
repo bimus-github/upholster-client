a290c3c617b6218f7817982d20924696e73cd8cc
bimus-github <bimus2022@gmail.com>
2023-09-29 09:43:04 +0500

    setting up react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from "react";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
 
 function App() {
-  return (
-    <main>
-      <section>Section 1</section>
-    </main>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
