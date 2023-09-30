d3cd665fbb9f0990aa2815c577b2a38fbcad9712
bimus-github <bimus2022@gmail.com>
2023-09-30 18:00:09 +0500

    adding contact

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,5 +2,6 @@ import About from "./about";
 import Navbar from "./navbar";
 import Services from "./services";
 import Designs from "./designs";
+import Contact from "./contact";
 
-export { About, Navbar, Services, Designs };
+export { About, Navbar, Services, Designs, Contact };
