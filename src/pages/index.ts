2b560df9b406acc38a0ff0584ef18e6be2665aa5
bimus-github <bimus2022@gmail.com>
2023-09-30 13:35:37 +0500

    adding designs page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,6 @@
 import About from "./about";
 import Navbar from "./navbar";
 import Services from "./services";
+import Designs from "./designs";
 
-export { About, Navbar, Services };
+export { About, Navbar, Services, Designs };
