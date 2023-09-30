7c8907cbb9c9524d4ef4407d9bf6235d6ba3e1d2
bimus-github <bimus2022@gmail.com>
2023-09-30 13:38:41 +0500

    select input component

---
+++ src/components/designs/SelectInput.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import ClearIcon from "@mui/icons-material/Clear";
+
+interface SelectInputProps {
+  title: string;
+  oprions: string[];
+  selectedItemName: string;
+  setSelectedItemName: Function;
+  loading: boolean;
+}
+
+function SelectInput({
+  title,
+  oprions,
+  selectedItemName,
+  setSelectedItemName,
+  loading,
+}: SelectInputProps) {
+  return (
+    <div className={styles.main}>
+      <fieldset className={styles.fieldset}>
+        <legend className={styles.legend}>
+          <cite>{title}</cite>
+        </legend>
+
+        <input
+          value={selectedItemName}
+          onChange={(e) => setSelectedItemName(e.target.value)}
+          className={styles.input}
+          type="text"
+          list={title}
+        />
+        {selectedItemName.length > 0 && (
+          <button
+            onClick={() => setSelectedItemName("")}
+            className="p-2 rounded-full hover:bg-gray-200"
+          >
+            <ClearIcon />
+          </button>
+        )}
+        <datalist id={title}>
+          {!loading &&
+            oprions.map((item, index) => <option key={index} value={item} />)}
+          {loading && <option className="animate-pulse">Loading...</option>}
+        </datalist>
+      </fieldset>
+    </div>
+  );
+}
+
+export default SelectInput;
+
+const styles = {
+  main: " w-full flex items-start justify-center",
+  fieldset:
+    "w-[300px] sm:w-[400px] md:w-[500px] lg:w-[600px] border px-4 py-2 rounded-lg shadow-lg flex",
+  legend: "font-semibold",
+  input: "w-full focus:outline-none p-2",
+};
